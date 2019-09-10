7783c995cdeb91ace1e2b1a2753b6429ba6529cc
Chris Karani <chriskarani@Chriss-MacBook-Pro.local>
2019-09-11 02:40:42 +0300

    imported networking modules

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# currencyRates
+# Currency Rates
+ Uses the `fixer.io` API to retrieve currency rates data
+ 
+ ### Running the project
+ create an account at `https://fixer.io/` and get an API Key to start using the API
+ 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate futures;
+extern crate hyper;
+extern crate tokio_core;
+
+use futures::{Future};
+use hyper::{Client, Uri};
+use tokio_core::reactor::Core;
+
+
+const API_KEY: &str = "7e8bd3b93485d813085a941b5a0cfc1e";
+const API_URL : &str = "http://data.fixer.io/api/";
+
+
+
+
 fn main() {
     println!("Hello, world!");
 }
